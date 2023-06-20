fc4d31ea0b7976e2700cf4250ff197cb7e4db62f
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2023-06-20 11:50:32 +0900

    Day1_10) Create the List of Landmarks

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 이예준 on 2023/06/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
