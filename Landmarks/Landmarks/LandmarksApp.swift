e3f54898b9ca148ba4249cc2ba150bc788766e2a
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2023-06-20 11:19:05 +0900

    Day1_1) Create a New Project & Explore the Canvas

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 이예준 on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
