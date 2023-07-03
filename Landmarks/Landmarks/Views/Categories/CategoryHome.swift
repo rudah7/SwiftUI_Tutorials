7cbe1edd5f0ea646bfebffd61de001da392249ff
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2023-07-04 04:34:20 +0900

    Day3_1) Add a Category View

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by 이예준 on 2023/07/04.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
