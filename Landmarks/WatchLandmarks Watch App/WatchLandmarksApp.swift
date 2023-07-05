fe75aedbb4049f48ffe14f6395edbef3fd45d7d6
Yejun Lee <107831192+rudah7@users.noreply.github.com>
2023-07-06 01:44:01 +0900

    Day4_5) Add a watchOS Target

---
+++ Landmarks/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by 이예준 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
